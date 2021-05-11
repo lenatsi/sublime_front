261078713a496fe285792bc1cde176a83c18ee9a
lenatsi <tsitukhinaalena@gmail.com>
2021-05-11 12:03:01 +0200

    First steps

---
+++ src/app/Job-detail/Job-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Job-detail',
+  templateUrl: './Job-detail.component.html',
+  styleUrls: ['./Job-detail.component.css']
+})
+export class JobDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
